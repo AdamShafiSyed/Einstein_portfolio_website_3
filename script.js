1cb1ebb4339b80ae97834e7bfab93a103f3e05d2
Adam Shafi Syed <shafiadam18@gmail.com>
2023-03-10 22:34:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+fetch("https://upadhayay.github.io/db.json")
+.then(function (response) {
+    return response.json();
+})
+.then(function (db) {
+    let placeholder = document.querySelector(".papers_container");
+    let out = "";
+    for (let i = 0; i < db.books.length; i++) {
+        out += `
+        <div class="paper">
+            <h2>Title:${db.books[i].title} (${db.books[i].year})</h2>
+            <a href="#" class="read_more">Read More</a>
+        </div>
+                `;
+    }
+
+    placeholder.innerHTML = out;
+});
